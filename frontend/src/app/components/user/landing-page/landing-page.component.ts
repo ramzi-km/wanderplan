8415ecb0705d1f538908e1db515edad9063163a0
Ramzi K.M <ramzikm3@gmail.com>
2023-08-12 00:51:36 +0530

    landing page updated and some minor changes

--- frontend/src/app/components/user/landing-page/landing-page.component.ts
+++ frontend/src/app/components/user/landing-page/landing-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  @ViewChild('exampleSection') exampleSection!: ElementRef;
+
+  scrollTo(sectionId: string): void {
+    let sectionElement: HTMLElement | undefined;
+
+    if (sectionId === 'exampleSection') {
+      sectionElement = this.exampleSection.nativeElement;
+    }
+
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
