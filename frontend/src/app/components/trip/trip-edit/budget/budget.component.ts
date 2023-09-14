53b4afabae41b06456c5cc2ed45de329214a6244
Ramzi K.M <ramzikm3@gmail.com>
2023-09-14 11:27:48 +0530

    :lipstick: working on ui

--- frontend/src/app/components/trip/trip-edit/budget/budget.component.ts
+++ frontend/src/app/components/trip/trip-edit/budget/budget.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Trip } from 'src/app/interfaces/trip.interface';
 
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
-  styleUrls: ['./budget.component.scss']
+  styleUrls: ['./budget.component.scss'],
 })
 export class BudgetComponent {
-
+  @Input() trip: Trip | undefined;
 }
